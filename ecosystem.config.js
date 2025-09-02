3aa4b5291b721a34e80ac43a4de7e8ea35e5695b
chientt <tranthechien2012@gmail.com>
2025-09-02 18:46:21 +0700

    adding ci/cd

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+// PM2 configuration file for web-29d-admin
+module.exports = {
+  apps: [{
+    name: 'spa-admin',
+    script: 'npm',
+    args: 'start -- -p 8888',
+    cwd: '/var/www/spa-admin',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 8888
+    }
+  }]
+};
