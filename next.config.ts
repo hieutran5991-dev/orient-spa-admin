1fa27d106c8ec2e25875cd5ff242b31bf630fda6
Tam Bui <n0bito@tambl.local>
2025-09-06 20:16:52 +0700

    Add image storage permission config

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,14 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
+        hostname: process.env.NEXT_STORAGE_HOST!,
+        port: process.env.NEXT_STORAGE_PORT!,
         pathname: '/storage/**',
       },
       {
         protocol: 'https',
-        hostname: 'localhost',
-        port: '8000',
+        hostname: process.env.NEXT_STORAGE_HOST!,
+        port: process.env.NEXT_STORAGE_PORT!,
         pathname: '/storage/**',
       },
     ],
