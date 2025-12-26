5bb22f7d9b370b172c56d2800fe8b531c1499a20
HieuTT <hieutt@ant.games>
2025-12-27 02:16:41 +0700

    sửa ui creating booking

--- src/components/bookings/CreateBookingPageForm.tsx
+++ src/components/bookings/CreateBookingPageForm.tsx
@@ -401,7 +401,7 @@ export default function CreateBookingPageForm() {
           </div>
 
           {/* VN Phone Number and App ID */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             {/* VN Phone Number */}
             <div>
               <PhoneInputWithPrefix
