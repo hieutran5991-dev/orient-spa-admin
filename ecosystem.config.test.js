3dab1467793c7a85f22db9c9a1e597ffaaa7cf5d
HieuTT <hieutt@ant.games>
2025-12-29 21:57:55 +0700

    deploy test

---
+++ ecosystem.config.test.js
@@ -0,0 +1,18 @@
+// PM2 configuration file for spa-admin-test
+module.exports = {
+  apps: [{
+    name: 'spa-admin-test',
+    script: 'npm',
+    args: 'start -- -p 8889',
+    cwd: '/var/www/spa-admin-test',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 8889
+    }
+  }]
+};
+
